bcbdf0a3ed71c737df44a90fd15140e03c50535e
Lesly <leslyr562@gmail.com>
2021-11-02 18:36:15 -0500

    added connection

--- server.js
+++ server.js
@@ -1,10 +1,24 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const mysql = require('mysql2')
 
 // Express middleware
 app.use(express.urlencoded({ extended: false}));
 app.use(express.json());
+
+//Connect database 
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+         // Your MySQL username,
+         user: 'root',
+          // Your MySQL password
+          password: 'abc123',
+          database: 'election'
+    },
+    console.log('Connected to the election database.')
+)
 
 // Default response for any other request (Not Found)
 app.use((req, res)=> {
