538cd4dd2a36f60622c7835f6b7ec1a969bd7367
Lesly <leslyr562@gmail.com>
2021-11-05 03:22:07 -0500

    added candidate with post

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const mysql = require('mysql2')
+const mysql = require('mysql2');
+const inputCheck = require('./utils/inputCheck');
 
 // Express middleware
 app.use(express.urlencoded({ extended: false }));
@@ -72,6 +73,29 @@ app.delete('/api/candidate/:id', (req, res) => {
                 id: req.params.id
             });
         }
+    });
+});
+
+//create candidate
+app.post('/api/candidate', ({ body }, res) => {
+    const errors = inputCheck(body, 'first_name', 'last_name', 'industry_connected');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+    const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+  VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.industry_connected];
+
+    db.query(sql, params, (err, result) => {
+        if (err) {
+            res.status(400).json({ error: err.message });
+            return;
+        }
+        res.json({
+            message: 'success',
+            data: body
+        });
     });
 });
 
